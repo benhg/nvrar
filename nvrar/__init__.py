0dbc705e2b10a6680d0db3f3a083d406812ed6fe
Prajwal Singhania <prajwal1210@gmail.com>
2025-10-01 16:28:12 -0700

    Changed package structured; Added LICENSE and README

--- nvrar/__init__.py
+++ nvrar/__init__.py
@@ -1,10 +1,9 @@
-from .comm import NVSHMEMCommunicator, NVSHMEMCommHandler
-
 try:
     from . import nvshmem_comm_cuda  # available if NVSHMEM build succeeded
-    HAS_NVSHMEM = True
+    from .comm import NVRARCommunicator
+    NVRAR_AVAILABLE = True
 except Exception:
     nvshmem_comm_cuda = None  # type: ignore
-    HAS_NVSHMEM = False
+    NVRAR_AVAILABLE = False
 
 
--- nvrar/comm.py
+++ nvrar/comm.py
@@ -2,28 +2,7 @@ import torch
 import torch.distributed as dist
 from . import nvshmem_comm_cuda
 
-
-class NVSHMEMCommHandler:
-    process_group_to_idx = {}
-    idx_to_comm = {}
-    num_comms = 0
-
-    @staticmethod
-    def create_communicator_from_process_group(
-        process_group: torch.distributed.ProcessGroup,
-    ) -> int:
-        if process_group not in NVSHMEMCommHandler.process_group_to_idx:
-            NVSHMEMCommHandler.process_group_to_idx[process_group] = NVSHMEMCommHandler.num_comms
-            NVSHMEMCommHandler.idx_to_comm[NVSHMEMCommHandler.num_comms] = NVSHMEMCommunicator(process_group)
-            NVSHMEMCommHandler.num_comms += 1
-        return NVSHMEMCommHandler.process_group_to_idx[process_group]
-
-    @staticmethod
-    def get_communicator_from_idx(idx: int):
-        return NVSHMEMCommHandler.idx_to_comm[idx]
-
-
-class NVSHMEMCommunicator:
+class NVRARCommunicator:
     def __init__(self, process_group: torch.distributed.ProcessGroup):
         rank = torch.distributed.get_rank(process_group)
         nranks = torch.distributed.get_world_size(process_group)
@@ -42,7 +21,7 @@ class NVSHMEMCommunicator:
         unique_id = uid_gpu.to("cpu")
 
         self.comm_wrapper = nvshmem_comm_cuda.NVSHMEMCommWrapper(rank, nranks, device, unique_id)
-        print(f"NVSHMEMCommunicator created for process group {process_group} with rank {rank} and nranks {nranks}")
+        print(f"NVRARCommunicator created for process group {process_group} with rank {rank} and nranks {nranks}")
 
         self.comm_wrapper.set_kernel_params(4, 256, 16384)
 
